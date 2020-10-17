0d9ec508d92e1886b43ad432f44d0d507d02bc5f
MedJelidi <mohammedjelidi05@gmail.com>
2020-10-17 11:13:02 +0100

    Homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { ExamModule } from './modules/exam.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
